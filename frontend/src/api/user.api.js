fcb2b49b2bdca0edb5f70b637cf5693449960c23
Anuj Negi <anujnegi9997@gmail.com>
2026-07-30 21:24:57 +0530

    user api created in frontend

--- frontend/src/api/user.api.js
+++ frontend/src/api/user.api.js
@@ -1,25 +1,34 @@
-import api from "./axios.js";
+import api from "./axios";
 
 
-/**
- * complete profile
- */
-export const completeProfile = async(formdata)=>{
-    const response = await api.patch("/users/compele-profile", formdata,{
-         headers: {
-        "Content-Type": "multipart/form-data",
-        },
-    });
-    return response.data;
+export const getProfileApi = async () => {
+  const response = await api.get("/users/profile");
+  return response.data;
 };
 
-/**
- * change password
- */
-export const changePassword = async (passwordData) => {
-    const response = await api.patch("/users/change-password", {
-        passwordData,
-    });
 
-    return response.data;
+export const updateProfileApi = async (formData) => {
+  const response = await api.patch("/users/update-profile", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return response.data;
+};
+
+export const completeProfileApi = async (formData) => {
+  const response = await api.patch("/users/complete-profile", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return response.data;
+};
+
+export const changePasswordApi = async (formData) => {
+  const response = await api.patch("/users/change-password", formData);
+
+  return response.data;
 };
