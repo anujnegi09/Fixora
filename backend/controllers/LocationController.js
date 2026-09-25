a2926882d8c61d73d0dc131aa24b9de98c0e6f29
Anuj Negi <anujnegi9997@gmail.com>
2026-09-25 20:19:09 +0530

    replace Nominatim with LocationIQ for Location Controller

--- backend/controllers/LocationController.js
+++ backend/controllers/LocationController.js
@@ -4,71 +4,46 @@ import apiError from "../utils/apiError.js";
 import apiResponse from "../utils/apiResponse.js";
 
 export const reverseGeocode = asyncHandler(async (req, res) => {
+  const { lat, lng } = req.query;
 
-    const { lat, lng } = req.query;
+  if (!lat || !lng) {
+    throw new apiError(400, "Latitude and Longitude are required");
+  }
 
-    if (!lat || !lng) {
-        throw new apiError(400, "Latitude and Longitude are required");
-    }
+  const { data } = await axios.get("https://us1.locationiq.com/v1/reverse", {
+    params: {
+      key: process.env.LOCATIONIQ_API_KEY,
+      lat,
+      lon: lng,
+      format: "json",
+      addressdetails: 1,
+    },
+  });
 
-    const { data } = await axios.get(
-        "https://nominatim.openstreetmap.org/reverse",
-        {
-            params: {
-                format: "jsonv2",
-                lat,
-                lon: lng,
-            },
-            headers: {
-                "User-Agent": "Fixora/1.0 (anujnegi9997@gmail.com)",
-                "Accept-Language": "en",
-            },
-        }
-    );
-
-    return res.status(200).json(
-
-        new apiResponse(
-            200,
-            data,
-            "Location fetched successfully"
-        )
-
-    );
-
+  return res
+    .status(200)
+    .json(new apiResponse(200, data, "Location fetched successfully"));
 });
 
-
 export const searchLocation = asyncHandler(async (req, res) => {
+  const { q } = req.query;
 
-    const { q } = req.query;
+  if (!q) {
+    throw new apiError(400, "Search query is required");
+  }
 
-    if (!q) {
-        throw new apiError(400, "Search query is required");
-    }
+  const { data } = await axios.get("https://us1.locationiq.com/v1/search", {
+    params: {
+      key: process.env.LOCATIONIQ_API_KEY,
+      q,
+      format: "json",
+      addressdetails: 1,
+      limit: 5,
+      countrycodes: "in",
+    },
+  });
 
-    const { data } = await axios.get(
-        "https://nominatim.openstreetmap.org/search",
-        {
-            params: {
-                q,
-                format: "jsonv2",
-                addressdetails: 1,
-                limit: 5,
-            },
-            headers: {
-                "User-Agent": "Fixora/1.0 (anujnegi9997@gmail.com)",
-                "Accept-Language": "en",
-            },
-        }
-    );
-
-    return res.status(200).json(
-        new apiResponse(
-            200,
-            data,
-            "Locations fetched successfully"
-        )
-    );
-
+  return res
+    .status(200)
+    .json(new apiResponse(200, data, "Locations fetched successfully"));
 });
