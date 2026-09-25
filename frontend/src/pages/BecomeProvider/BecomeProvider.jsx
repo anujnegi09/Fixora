c8678cab4d6d55ea279e7b003dfe76861c24590d
Anuj Negi <anujnegi9997@gmail.com>
2026-09-25 21:39:19 +0530

    bug fix

--- frontend/src/pages/BecomeProvider/BecomeProvider.jsx
+++ frontend/src/pages/BecomeProvider/BecomeProvider.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "../../components/common/Loading.jsx";
 import CreateServiceModal from "../../components/service/CreateServiceModal.jsx";
